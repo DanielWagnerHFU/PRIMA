353a8b9030f40de2dfa79e0d1503bb6f6fac21b1
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2020-04-29 15:45:12 +0200

    added diagram

--- Projects/Snake_Custom/Main.ts
+++ Projects/Snake_Custom/Main.ts
@@ -28,26 +28,50 @@ namespace Snake_Custom {
     }
 
     function update(): void {
-        node.mtxLocal.translateX(0.01);
         viewport.draw();
     }
 
     function createScene(): void {
-        node = new ƒ.Node("Quad");
-        node.addComponent(new ƒ.ComponentTransform(ƒ.Matrix4x4.TRANSLATION(new ƒ.Vector3(1, 0, 0))));
+        node = new SnakeBoard(10,10);
+        // node.addComponent(new ƒ.ComponentTransform(ƒ.Matrix4x4.TRANSLATION(new ƒ.Vector3(1, 0, 0))));
 
-        let mesh: ƒ.MeshQuad = new ƒ.MeshQuad();
-        let cmpMesh: ƒ.ComponentMesh = new ƒ.ComponentMesh(mesh);
-        node.addComponent(cmpMesh);
+        // let mesh: ƒ.MeshQuad = new ƒ.MeshQuad();
+        // let cmpMesh: ƒ.ComponentMesh = new ƒ.ComponentMesh(mesh);
+        // node.addComponent(cmpMesh);
         
-        let mtrSolidWhite: ƒ.Material = new ƒ.Material("SolidWhite", ƒ.ShaderUniColor, new ƒ.CoatColored(ƒ.Color.CSS("WHITE")));
-        let cmpMaterial: ƒ.ComponentMaterial = new ƒ.ComponentMaterial(mtrSolidWhite);
-        node.addComponent(cmpMaterial);
+        // let mtrSolidWhite: ƒ.Material = new ƒ.Material("SolidWhite", ƒ.ShaderUniColor, new ƒ.CoatColored(ƒ.Color.CSS("WHITE")));
+        // let cmpMaterial: ƒ.ComponentMaterial = new ƒ.ComponentMaterial(mtrSolidWhite);
+        // node.addComponent(cmpMaterial);
     }
 
     class SnakeBoard extends ƒ.Node {
+        private width: number;
+        private height: number;
+        private board: SnakeField[][];
+
         public constructor(width: number, height: number) {
             super("SnakeBoard");
+            this.width = width;
+            this.height = height;
+        }
+
+        public initializeBoard(): void {
+            for(let x: number = 0; x < this.width; x++) {
+                for(let y: number = 0; y < this.height; y++) {
+                    this.board[x][y] = new SnakeField_Empty();
+                    this.appendChild(this.board[x][y]);
+                }
+            }
+        }
+    }
+
+    abstract class SnakeField extends ƒ.Node {
+        
+    }
+
+    class SnakeField_Empty extends SnakeField {
+        public constructor() {
+            super("SnakeField_Empty");
         }
     }
 }
